83e20e273e759c429f7a61623419913736071b41
飯田剛通 <iidatsuyoshitoori@iidatsuyoshitoorinoMacBook-Air.local>
2022-05-04 18:42:23 +0900

    データベース接続とテーブル作成まで完了

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"project/omen/config/db"
+
+	"gorm.io/gorm"
 )
 
 func main() {
-	db.DbConnection()
+	var data *gorm.DB
+	data, err := db.DbOpen()
+	if err != nil {
+		fmt.Printf("DB connection error: %v", err)
+	}
+	db.Migrate(data)
 }
